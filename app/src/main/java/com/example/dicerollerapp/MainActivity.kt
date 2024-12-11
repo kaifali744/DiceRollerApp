75f6aa164e0860e679da00c8f5a7188e74c07fb2
Mohammad Kaif Ali <kaifali744@gmail.com>
2024-12-11 14:29:29 +0530

    files upload
    all files upload

---
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.dicerollerapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView
+    lateinit var rollBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        rollBtn = findViewById(R.id.rollBtn)
+        rollBtn.setOnClickListener {
+            rollDice()
+        }
+
+        diceImg = findViewById(R.id.diceImg)
+
+    }
+
+    private fun rollDice() {
+
+        val imgResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImg.setImageResource(imgResource)
+    }
+}
